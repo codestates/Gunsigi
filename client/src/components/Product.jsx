db31831c42af5626c13a4608753de8ab9ae7984a
Jin-suyeon <wlstndus99@gmail.com>
2021-10-25 18:11:17 +0900

    [client] mock-up_01

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,7 +1,33 @@
+/* eslint-disable */
 import React from 'react';
+import '../styles/Product.scss';
 
-function Product() {
-  return <div></div>;
+function Product({ name, heart, star, reviews, img }) {
+  return (
+    <div className="Product_container">
+      <div className="Product_items">
+        <div>
+          <img src={img} alt={name} />
+        </div>
+        <div>
+          <span>{name}</span>
+          <img className="Product_heart" src={heart} alt="heart" />
+        </div>
+        <div>
+          <span>
+            <img className="Product_star" src={star} alt="star" />
+            <img className="Product_star" src={star} alt="star" />
+            <img className="Product_star" src={star} alt="star" />
+            <img className="Product_star" src={star} alt="star" />
+            <img className="Product_star" src={star} alt="star" />
+          </span>
+          <div>
+            <span>{`(${reviews})`}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,7 +1,93 @@
 import React from 'react';
+import '../styles/Review.scss';
 
-function Review() {
-  return <div></div>;
+function Review({
+  name,
+  profile,
+  nickname,
+  productId,
+  content,
+  date,
+  score,
+  isMine,
+  images,
+}) {
+  return (
+    <div className="Reviews_container">
+      <div className="Reviews_in">
+        <div className="Reviews_left">
+          {isMine ? (
+            <img
+              className="delete_left"
+              src="icons/icon_bin.svg"
+              alt="review_delete"
+            />
+          ) : null}
+          <img className="profile" src={profile} alt="profile_img" />
+          <div className="nameOrDate">
+            <div className="nickname">{nickname}</div>
+            <div className="date">{date}</div>
+          </div>
+          <div className="stars">
+            <img
+              className={
+                score >= 1 ? 'ProductDetail_icon_change' : 'ProductDetail_icon'
+              }
+              src="icons/icon_star_fill.svg"
+              alt="star"
+            />
+            <img
+              className={
+                score >= 2 ? 'ProductDetail_icon_change' : 'ProductDetail_icon'
+              }
+              src="icons/icon_star_fill.svg"
+              alt="star"
+            />
+            <img
+              className={
+                score >= 3 ? 'ProductDetail_icon_change' : 'ProductDetail_icon'
+              }
+              src="icons/icon_star_fill.svg"
+              alt="star"
+            />
+            <img
+              className={
+                score >= 4 ? 'ProductDetail_icon_change' : 'ProductDetail_icon'
+              }
+              src="icons/icon_star_fill.svg"
+              alt="star"
+            />
+            <img
+              className={
+                score > 4 ? 'ProductDetail_icon_change' : 'ProductDetail_icon'
+              }
+              src="icons/icon_star_fill.svg"
+              alt="star"
+            />
+          </div>
+        </div>
+        <div className="Reviews_right">
+          {isMine ? (
+            <img
+              className="delete"
+              src="icons/icon_bin.svg"
+              alt="review_delete"
+            />
+          ) : null}
+          <div className="name">
+            <span>제품명</span>
+            <span>{name}</span>
+          </div>
+          <div className="images">
+            {images.map((image) => (
+              <img src={image} alt="review_img" />
+            ))}
+          </div>
+          <div className="content">{content}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Review;
