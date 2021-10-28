ab0bb0db26cee7fad2629b38bcb04deb2f75a218
doldolma <hdy3405@gmail.com>
2021-10-28 12:35:50 +0900

    ADD S3모듈, 프사변경API

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const debug = require('debug')('app');
 const { User } = require('../models');
-const image = require('../modules/image');
+const s3 = require('../modules/image');
 
 module.exports = {
   get: async (req, res) => {
@@ -21,26 +21,21 @@ module.exports = {
       // 유저 정보가 없음
       return res.status(400).json({ Message: 'Invalid Token' });
     }
-    Object.keys(req.body).forEach((key) => {
-      if (key === 'profileImage') {
-        // 프로필 이미지 저장 일단 서버에 저장
-        console.log('uuid : ', image.save());
-        fs.writeFileSync(
-          'test.jpg',
-          req.body.profileImage.replace(/^data:image\/jpeg;base64,/, ''),
-          'base64',
-          (err) => {
-            if (err) {
-              debug(err);
-              return res.status(400).json({ message: 'Invalid Image' });
-            }
-          },
-        );
-      } else {
-        user[key] = req.body[key];
+
+    if (Object.prototype.hasOwnProperty.call(req.body, 'profileImage')) {
+      // 프로필 이미지저장, 이미 있으면 기존꺼 삭제, 안에 값이 비어있으면 삭제만
+      const imageName = await s3.save('profile', req.body.profileImage);
+      if (user.profileImage) {
+        // 기존 이미지 삭제
+        s3.delete(user.profileImage);
       }
+      user.profileImage = imageName;
+      delete req.body.profileImage;
+    }
+    Object.keys(req.body).forEach(async (key) => {
+      user[key] = req.body[key];
     });
-    user.save();
+    await user.save();
     return res.json({
       message: 'success to modify userInfo',
       userInfo: user.json(),
--- server/modules/image.js
+++ server/modules/image.js
@@ -1,13 +1,58 @@
 const uuid = require('uuid').v4;
+const AWS = require('aws-sdk');
+const debug = require('debug')('app');
 
 const v4 = () => uuid().replace(/-/g, '').slice(0, 15);
 
+const {
+  ACCESS_KEY_ID,
+  ACCESS_SECRET,
+  AWS_REGION,
+  BUCKET,
+} = process.env;
+
+// Configure AWS to use promise
+AWS.config.setPromisesDependency(require('bluebird'));
+
+AWS.config.update({
+  accessKeyId: ACCESS_KEY_ID,
+  secretAccessKey: ACCESS_SECRET,
+  region: AWS_REGION,
+});
+const s3 = new AWS.S3();
+
 module.exports = {
-  save: (path) => {
+  save: async (path, image) => {
     // CDN서버에 이미지를 저장한다.
-    return v4();
+    const base64Data = new Buffer.from(
+      image.replace(/^data:image\/\w+;base64,/, ''),
+      'base64',
+    );
+    const type = image.split(';')[0].split('/')[1];
+    const uid = v4();
+    const params = {
+      Bucket: BUCKET,
+      Key: `${path}/${uid}.${type}`,
+      Body: base64Data,
+      ACL: 'public-read',
+      ContentEncoding: 'base64',
+      ContentType: `image/${type}`,
+    };
+
+    let key = '';
+    try {
+      const { Key } = await s3.upload(params).promise();
+      key = Key;
+    } catch (error) {
+      debug(error);
+    }
+    return key;
   },
-  delete: (path) => {
+  delete: (Key) => {
     // CDN서버에 있는 이미지를 삭제한다.
+    s3.deleteObject({ Bucket: BUCKET, Key }, (err, data) => {
+      if (err) debug('err', err);
+      else debug(data);
+    });
   },
 };
