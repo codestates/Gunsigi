c000cf20b1f1eca67dbecd199d3093e2f4e184fb
Jin-suyeon <wlstndus99@gmail.com>
2021-10-31 14:32:18 +0900

    fix: pull 이후 myreviews 충돌해결 및 Login, Signup Modal, Top Button, Search 수정

--- client/src/components/SignupModal.jsx
+++ client/src/components/SignupModal.jsx
@@ -20,7 +20,7 @@ function SignupModal({ setOpenSignup }) {
     const signup = document.getElementById('SignupModal_Signup_change');
 
     if (e.target.innerText === '로그인') {
-      loginSignupImg.style.left = '380px';
+      loginSignupImg.style.left = '370px';
       loginSignupImg.style.borderRadius = '0 6px 6px 0';
       login.style.display = 'none';
       signup.style.display = 'flex';
@@ -40,29 +40,31 @@ function SignupModal({ setOpenSignup }) {
       ref={closeSignupEl}
       className="SignupModal_container"
     >
-      <div className="SignupModal_in">
-        <div id="SignupModal_img_Login_Signup">
-          <div className="img">
-            <div id="SignupModal_Signup_change">
-              <div>아직 회원가입을 안하셨나요?</div>
-              <button onClick={(e) => LoginSignupChange(e)} type="button">
-                회원가입
-              </button>
-            </div>
-            <div id="SignupModal_Login_change">
-              <div>이미 회원가입을 하셨나요?</div>
-              <button onClick={(e) => LoginSignupChange(e)} type="button">
-                로그인
-              </button>
+      <div className="SignupModal">
+        <div className="SignupModal_in">
+          <div id="SignupModal_img_Login_Signup">
+            <div className="img">
+              <div id="SignupModal_Signup_change">
+                <div>아직 회원가입을 안하셨나요?</div>
+                <button onClick={(e) => LoginSignupChange(e)} type="button">
+                  회원가입
+                </button>
+              </div>
+              <div id="SignupModal_Login_change">
+                <div>이미 회원가입을 하셨나요?</div>
+                <button onClick={(e) => LoginSignupChange(e)} type="button">
+                  로그인
+                </button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="SignupModal_Login">
-          <Login />
-        </div>
-        <div className="SignupModal_Signup">
-          <Signup />
+          <div className="SignupModal_Login">
+            <Login />
+          </div>
+          <div className="SignupModal_Signup">
+            <Signup />
+          </div>
         </div>
       </div>
     </div>
